8c003b36f8fc715abd13ce17d13b4ae57109a3db
Achi00 <toolvlg@gmail.com>
2026-04-27 23:20:59 +0400

    test hash

--- Shazam.Application/Peaks/PeakHashing.cs
+++ Shazam.Application/Peaks/PeakHashing.cs
@@ -2,6 +2,14 @@
 {
     public class PeakHashing
     {
+        public uint CalculateHash(List<Fingerprint> fp)
+        {
+            foreach (var item in fp)
+            {
+                uint hashData = (uint)((item.Freq1 << 20) | (item.Freq2 << 10) | item.DeltaTime);
+            }
 
+            return 0;
+        }
     }
 }
