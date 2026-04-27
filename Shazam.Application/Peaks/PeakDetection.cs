472fb4a0fcf5e8eb3a1e95b198d15ceff1320d1a
Achi00 <toolvlg@gmail.com>
2026-04-27 13:50:59 +0400

    looping over spactogram data

--- Shazam.Application/Peaks/PeakDetection.cs
+++ Shazam.Application/Peaks/PeakDetection.cs
@@ -4,7 +4,54 @@
     {
         public List<(int time, int freq)> FindPeaks(List<float[]> spectrogram)
         {
-            return [];
+            int timeRadius = 5;
+            int freqRadius = 10;
+            float threshold = -5f;
+
+            var peaks = new List<(int, int)>();
+            // goes front in music play
+            for (int t = timeRadius; t < spectrogram.Count - timeRadius; t++)
+            {
+                var frame = spectrogram[t];
+                // local max peak
+                for (int f = freqRadius; f < frame.Length; f++)
+                {
+                    var current = frame[f];
+
+                    if (current < threshold)
+                    {
+                        continue;
+                    }
+
+                    bool isPeak = true;
+
+                    // find neighbor frames
+                    for (int dt = -timeRadius; dt <= timeRadius && isPeak; dt++)
+                    {
+                        var neighborFrame = spectrogram[t + dt];
+
+                        for (int df = -freqRadius; df <= freqRadius; df++)
+                        {
+                            if (dt == 0 && df == 0)
+                            {
+                                continue;
+                            }
+
+                            if (neighborFrame[f + df] > current)
+                            {
+                                isPeak = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isPeak)
+                    {
+                        peaks.Add((t, f));
+                    }
+                }
+            }
+            return peaks;
         }
     }
 }
