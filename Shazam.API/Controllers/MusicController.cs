5ee9a663f8490dc2d4444e03ea92054a6fe8c1f5
Achi00 <toolvlg@gmail.com>
2026-05-04 21:39:50 +0400

    music data pipeline, get youtube audio streams

---
+++ Shazam.API/Controllers/MusicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shazam.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult StoreMusic()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Shazam.Application/Services/DownloadYoutubeAudio.cs
@@ -0,0 +1,28 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace Shazam.Application.Services
+{
+    // this is part of data collecting pipeline
+    public class DownloadYoutubeAudio
+    {
+        public async Task DownloadAudio(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new ArgumentException("Youtube video url is required");
+            }
+            using var youtubeClinet = new YoutubeClient();
+
+            // get available streams
+            var stream = await youtubeClinet.Videos.Streams.GetManifestAsync(url);
+
+            // get audio only stream
+            var streamInfo = stream.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            // download stream
+            await youtubeClinet.Videos.Streams.DownloadAsync(streamInfo, $"audio.{streamInfo.Container}");
+
+        }
+    }
+}
