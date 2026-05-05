dee6b511d09f321b18ffc280b0cfad28a6a5f5da
Achi00 <toolvlg@gmail.com>
2026-05-05 23:04:07 +0400

    finishe song repo, add song service interface

---
+++ Shazam.Application/Interfaces/Service/Song/ISongService.cs
@@ -0,0 +1,13 @@
+﻿using Shazam.Application.DTOs.Song;
+
+namespace Shazam.Application.Interfaces.Service.Song
+{
+    public interface ISongService
+    {
+        Task<SongResponse> GetAllAsync(CancellationToken ct = default);
+        Task<IEnumerable<SongResponse>> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<SongResponse> AddSongAsync(AddSongRequest dto, CancellationToken ct = default);
+        Task RemoveSongAsync(int id, CancellationToken ct = default);
+        Task UpdateSongAsync(int id, CancellationToken ct = default);
+    }
+}
---
+++ Shazam.Application/Services/Song/SongService.cs
@@ -0,0 +1,33 @@
+﻿using Shazam.Application.DTOs.Song;
+using Shazam.Application.Interfaces.Service.Song;
+
+namespace Shazam.Application.Services.Song
+{
+    public class SongService : ISongService
+    {
+        public Task<SongResponse> AddSongAsync(AddSongRequest dto, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SongResponse> GetAllAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SongResponse>> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveSongAsync(int id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateSongAsync(int id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
