18f64074672a1d0ea79286f522f468b709de0d41
Achi00 <toolvlg@gmail.com>
2026-05-06 18:49:50 +0400

    add unit of work layer

---
+++ Shazam.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Shazam.Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken ct = default);
+        Task CommitTransactionAsync(CancellationToken ct = default);
+        Task RollbackTransactionAsync(CancellationToken ct = default);
+        Task<int> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
--- Shazam.Application/Services/Song/SongService.cs
+++ Shazam.Application/Services/Song/SongService.cs
@@ -1,10 +1,20 @@
 ﻿using Shazam.Application.DTOs.Song;
+using Shazam.Application.Interfaces;
+using Shazam.Application.Interfaces.Repository;
 using Shazam.Application.Interfaces.Service.Song;
 
 namespace Shazam.Application.Services.Song
 {
     public class SongService : ISongService
     {
+        private readonly ISongRepository _songRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SongService(ISongRepository songRepository, IUnitOfWork unitOfWork)
+        {
+            _songRepository = songRepository;
+            _unitOfWork = unitOfWork;
+        }
         public Task<SongResponse> AddSongAsync(AddSongRequest dto, CancellationToken ct = default)
         {
             throw new NotImplementedException();
