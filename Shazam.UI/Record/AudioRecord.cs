9be2e75f40d956c2b5c9f71731cb84367a3e9096
Achi00 <toolvlg@gmail.com>
2026-05-10 22:46:59 +0400

    set up audio capture

---
+++ Shazam.UI/Record/AudioRecord.cs
@@ -0,0 +1,15 @@
+﻿using NAudio.Wave;
+
+namespace Shazam.UI.Record
+{
+    public class AudioRecord
+    {
+        public void RecordAudio()
+        {
+            var waveIn = new WaveInEvent();
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+
+            var writer = new WaveFileWriter("recording.wav", waveIn.WaveFormat);
+        }
+    }
+}
