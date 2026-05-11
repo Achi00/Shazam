04904c032e9151939a819282d8b4d555ada5df65
Achi00 <toolvlg@gmail.com>
2026-05-11 19:07:28 +0400

    record audio

--- Shazam.UI/Record/AudioRecord.cs
+++ Shazam.UI/Record/AudioRecord.cs
@@ -2,14 +2,33 @@
 
 namespace Shazam.UI.Record
 {
-    public class AudioRecord
+    public static class AudioRecord
     {
-        public void RecordAudio()
+        public static void RecordAudio()
         {
             var waveIn = new WaveInEvent();
             waveIn.WaveFormat = new WaveFormat(44100, 1);
 
             var writer = new WaveFileWriter("recording.wav", waveIn.WaveFormat);
+
+            // incoming data
+            waveIn.DataAvailable += (s, e) =>
+            {
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+            };
+
+            // stop recording
+            waveIn.RecordingStopped += (s, e) =>
+            {
+                waveIn.Dispose();
+            };
+
+            waveIn.StartRecording();
+            Console.WriteLine("• Recording audio");
+            Console.WriteLine("Press any button to stop");
+            Console.ReadLine();
+            waveIn.StopRecording();
+            waveIn.Dispose();
         }
     }
 }
